470a21a4340e9a2636d0fbea1c781a6d4eaea082
Grzegorz Dokowicz <grzegorz.dokowicz@gmail.com>
2020-02-16 15:13:05 +0100

    Added new idea for Observer Pattern, modified all js files

---
+++ src/core/observer.js
@@ -0,0 +1,10 @@
+class Observer {
+    constructor(state) {
+        state.registerObserver(this)
+    }
+
+    notify() {
+    }
+}
+
+export default Observer
---
+++ src/views/state/index.js
@@ -0,0 +1,19 @@
+import Store from '../../core/store'
+
+class State extends Store {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    update(data = {}) {
+        this.state = Object.assign(this.state, data)
+        this.notifyObservers(this.state)
+    }
+
+    get() {
+        return this.state
+    }
+}
+
+export default State;

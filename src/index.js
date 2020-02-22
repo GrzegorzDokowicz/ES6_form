64f1482d59d4d73ab2424f2d838f63468609e935
Grzegorz Dokowicz <grzegorz.dokowicz@gmail.com>
2020-02-22 16:18:41 +0100

    Add fetch on Mocky data

--- src/index.js
+++ src/index.js
@@ -2,24 +2,21 @@
 import $ from 'jquery';
 import State from './views/state';
 import List from './views/list';
+import Requests from "./core/requests";
 
 document.addEventListener("DOMContentLoaded", function () {
     const initialStateData = {
-        photos: [{name: 'John', lastname: 'Doe'}]
+        photos: []
     };
     const application = new State(initialStateData);
+    console.log(application)
+    let dummyData = new Requests().get('http://www.mocky.io/v2/5e513b0e3100008500415951').then(data => data.forEach(element => application.state.photos.push(element)));
+    setTimeout(()=> new List(application, '.js-photo-list'), 3000)
 
-    new List(application, '.js-photo-list');
-
-    setTimeout(() => application.update((lastState) => ({
-        ...lastState,
-        photos: [
-            ...lastState.photos,
-            {
-                name: 'Marek',
-            }
-        ]
-    })), 3000);
+    // setTimeout(() => application.update((lastState) => ({
+    //     ...lastState,
+    //     photos: [...lastState.photos, dummyData]
+    // })), 3000);
 })
 
 
--- src/views/list/index.js
+++ src/views/list/index.js
@@ -1,23 +1,27 @@
-import { template } from 'lodash';
+import {template} from 'lodash';
 import ViewTemplate from '../viewTemplate';
 
 class List extends ViewTemplate {
-  constructor(state, element) {
-    super(state, element);
-  }
+    constructor(state, element) {
+        super(state, element);
+    }
 
-  createComponent() {
-    console.log("From create", this.storeData);
-    const compiled = template('<ul class="<%- className %>"><% _.forEach(users, function(user) { %><li><%- console.log(user.name) %></li><% }); %></ul>');
+    createComponent() {
+        console.log("From create", this.storeData.photos);
+        const tableTemplate = '<table class="<%- tableClassName %>"><thead><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Photo</th></tr></thead>' +
+            '<tbody class="<%- tableBodyClassName %>"><%_.forEach(users, function (user) { %><tr><td><%- user.id %></td> <td><%-user.first_name %></td> <td><%-user.last_name %></td> <td><%-user.email %></td> <td><img src="<%- user.url %>" alt="User Photo"></td>  <% }); %></tr></tbody></table>'
+        const compiled = template(tableTemplate);
 
-    return compiled({
-      className: 'userList',
-      users: this.storeData.photos,
-    });
-  }
+        return compiled({
+            tableClassName: 'authorsList',
+            tableBodyClassName: 'authorsListTableBody',
+            users: this.storeData.photos,
+        });
+    }
 
-  render() {
-    document.querySelector(this.element).innerHTML = this.createComponent();
-  }
+    render() {
+        document.querySelector(this.element).innerHTML = this.createComponent();
+    }
 }
+
 export default List;

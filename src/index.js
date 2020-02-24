010299cdb67664c41bff511c6e9180a8daa959d2
Grzegorz Dokowicz <grzegorz.dokowicz@gmail.com>
2020-02-24 16:29:41 +0100

    Create button class

--- src/index.js
+++ src/index.js
@@ -4,19 +4,15 @@ import State from './views/state';
 import List from './views/list';
 import Requests from "./core/requests";
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded",  () => {
     const initialStateData = {
         photos: []
     };
     const application = new State(initialStateData);
-    console.log(application)
-    let dummyData = new Requests().get('http://www.mocky.io/v2/5e513b0e3100008500415951').then(data => data.forEach(element => application.state.photos.push(element)));
+
+    new Requests().get('http://www.mocky.io/v2/5e513b0e3100008500415951').then(data => data.forEach(element => application.state.photos.push(element)));
     setTimeout(()=> new List(application, '.js-photo-list'), 3000)
 
-    // setTimeout(() => application.update((lastState) => ({
-    //     ...lastState,
-    //     photos: [...lastState.photos, dummyData]
-    // })), 3000);
 })
 
 
--- src/views/list/index.js
+++ src/views/list/index.js
@@ -7,9 +7,8 @@ class List extends ViewTemplate {
     }
 
     createComponent() {
-        console.log("From create", this.storeData.photos);
         const tableTemplate = '<table class="<%- tableClassName %>"><thead><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Photo</th></tr></thead>' +
-            '<tbody class="<%- tableBodyClassName %>"><%_.forEach(users, function (user) { %><tr><td><%- user.id %></td> <td><%-user.first_name %></td> <td><%-user.last_name %></td> <td><%-user.email %></td> <td><img src="<%- user.url %>" alt="User Photo"></td>  <% }); %></tr></tbody></table>'
+            '<tbody class="<%- tableBodyClassName %>"><%_.forEach(users, function (user) { %><tr><td><%- user.id %></td> <td><%-user.first_name %></td> <td><%-user.last_name %></td> <td><%-user.email %></td> <td><img src="<%- user.URL %>" alt="User Photo"></td>  <% }); %></tr></tbody></table>'
         const compiled = template(tableTemplate);
 
         return compiled({
